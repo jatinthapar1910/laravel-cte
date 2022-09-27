33d3f7162e67efaaa708cd92c7367916e3fbcf17
Jatin Thapar <65127762+jatinthapar1910@users.noreply.github.com>
2022-09-27 10:15:23 +0530

    Added lateral joins.

--- src/Query/Traits/BuildsExpressionQueries.php
+++ src/Query/Traits/BuildsExpressionQueries.php
@@ -217,4 +217,37 @@ trait BuildsExpressionQueries
             $this->grammar->getBindingsForUpdate($this, $this->bindings, $values)
         ));
     }
+    
+    /**
+     * Add a lateral subquery join clause to the query.
+     *
+     * @param \Closure|\Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder|string $query
+     * @param \Closure|string $first
+     * @param string|null $operator
+     * @param string|null $second
+     * @psalm-suppress ImplicitToStringCast The doctype of join for $table is wrong
+     */
+    public function joinSubLateral($query, string $as, $first, $operator = null, $second = null, string $type = 'inner', bool $where = false): static
+    {
+        [$query, $bindings] = $this->createSub($query);
+
+        $expression = new Expression("lateral ({$query}) as {$this->grammar->wrapTable($as)}");
+
+        $this->addBinding($bindings, 'join');
+
+        return $this->join($expression, $first, $operator, $second, $type, $where);
+    }
+
+    /**
+     * Add a lateral subquery left join to the query.
+     *
+     * @param \Closure|\Illuminate\Database\Query\Builder|\Illuminate\Database\Eloquent\Builder|string $query
+     * @param \Closure|string $first
+     * @param string|null $operator
+     * @param string|null $second
+     */
+    public function leftJoinSubLateral($query, string $as, $first, $operator = null, $second = null): static
+    {
+        return $this->joinSubLateral($query, $as, $first, $operator, $second, 'left');
+    }
 }
